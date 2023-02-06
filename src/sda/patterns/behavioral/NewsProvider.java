60530f25d3dd3928882f70a11f08d6ada6c49e78
jwypysze <joanna.wypyszewska@gmail.com>
2023-02-06 20:59:48 +0100

    Wzorce behawioralne - TemplateMethod

---
+++ src/sda/patterns/behavioral/NewsProvider.java
@@ -0,0 +1,36 @@
+package sda.patterns.behavioral;
+
+public abstract class NewsProvider {
+    private String message;
+    /**
+     * metoda do implementacji
+     */
+    public abstract boolean authorize();
+
+    /**
+     * metoda - częśc algorytmu już zaimplementowana niezmienna
+     */
+    private final boolean sendMessage(){
+        System.out.println("Wysyłanie wiadomości " + message);
+        return true;
+    }
+
+    /**
+     * metoda do implementacji
+     */
+    public abstract boolean endConnection();
+
+    /**
+     * metoda z algorytmem niezmienna
+     */
+    public final void provideNews() {
+        if (authorize()) {
+            sendMessage();
+            endConnection();
+        }
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+}
---
+++ src/sda/patterns/behavioral/TemplateMethodDemoApp.java
@@ -0,0 +1,10 @@
+package sda.patterns.behavioral;
+
+public class TemplateMethodDemoApp {
+    public static void main(String[] args) {
+        NewsProvider newsProvider = new EmailNewsProvider("aaa@bbb.ccc");
+        newsProvider.setMessage("Bardzo ważny news: kończymy na dziś!");
+        newsProvider.provideNews();
+
+    }
+}
