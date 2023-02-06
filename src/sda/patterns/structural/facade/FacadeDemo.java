b67833c5d7713170532828979dc97d8ac7c863a1
jwypysze <joanna.wypyszewska@gmail.com>
2023-02-06 18:15:45 +0100

    Wzorce strukturalne - facade

---
+++ src/sda/patterns/structural/facade/FacadeDemo.java
@@ -0,0 +1,29 @@
+package sda.patterns.structural.facade;
+
+import java.sql.Connection;
+
+public class FacadeDemo {
+    public static void main(String[] args) {
+        traditionalCall();
+        facadeCall();
+    }
+    private static void traditionalCall(){
+        /**
+         * Tradycyjne korzystanie z klas powoduje, że programista "końcowy" używa naszego kodu
+         * niekoniecznie wiedząc co i jak używać, robi to żeby zadziałało żeby się kompilowalo
+         * niekoniecznie optymalnie albo zgodnie z założeniami autora kodu do generowania raportów.
+         */
+        Connection connection = MySQLReportGenerator.getMySQLDBConnection();
+        MySQLReportGenerator generator = new MySQLReportGenerator();
+        generator.generateHtmlReport("Customers", connection);
+        System.out.println("================================");
+        connection = OracleReportGenerator.getOracleDBConnection();
+        OracleReportGenerator generator1 = new OracleReportGenerator();
+        generator1.generateHtmlReport("Cars", connection);
+    }
+
+    private static void facadeCall(){
+        ReportGeneratorFacade.generateReport
+                (ReportGeneratorFacade.DBType.ORACLE, ReportGeneratorFacade.ReportType.PDF, "Customers");
+    }
+}
---
+++ src/sda/patterns/structural/facade/MySQLReportGenerator.java
@@ -0,0 +1,16 @@
+package sda.patterns.structural.facade;
+
+import java.sql.Connection;
+
+public class MySQLReportGenerator {
+    public static Connection getMySQLDBConnection(){
+        System.out.println("Tu łączymy się z bazą mySQL");
+        return null;
+    }
+    public void generatePDFReport(String tableName, Connection connection){
+        System.out.println("Generator raportu PDF");
+    }
+    public void generateHtmlReport(String tableName, Connection connection){
+        System.out.println("Generator raportu HTML");
+    }
+}
